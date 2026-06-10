9c4843444b94a707f7be854f5d9bf38f7739b02f
tbradsha <tbradsha@users.noreply.github.com>
2026-06-10 17:23:29 +0000

    PHPCS: Remove unused suppressions (#49443)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27292580137
    
    Upstream-Ref: Automattic/jetpack@dc72cffc5c1afb5b38a4494bd8715300346f4e98

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '8.6.0',
+      'ver' => '8.6.1-alpha1781111196',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
@@ -26,11 +26,11 @@ return array(
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.13',
+      'ver' => '0.4.14-alpha1781111196',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.42.0',
+      'ver' => '4.42.1-alpha1781111196',
     ),
   ),
 );
